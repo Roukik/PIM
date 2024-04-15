85ae6603f69ef1ec78e78a7643ac4898d604a95c
dienabadhb <d_diakhaby@etu-webschoolfactory.fr>
2024-04-15 22:42:28 +0200

    Button page d'ouverture reparer

--- PIM/src/App.jsx
+++ PIM/src/App.jsx
@@ -1,33 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-import './App.css'; // Assurez-vous que le chemin est correct
-
-// Créez un composant pour la page d'accueil
-const Accueil = () => {
-  return <h2>C'est la page d'accueil!</h2>;
-}
-
-// Créer un composant pour la page que vous souhaitez lier
-const AutrePage = () => {
-  return <h2>C'est une autre page!</h2>;
-}
+import './App.css'
+import {Link} from 'react-router-dom'
 
 function App() {
-  return (
-    <Router>
-      <div>
-        {/* Créez un lien vers la page d'accueil */}
-        <Link to="/accueil">
-          <button>PAGE D'OUVERTURE</button>
-        </Link>
 
-        {/* Définir la route de la page d'accueil */}
-        <Route path="/accueil" component={Accueil} />
-      </div>
-    </Router>
-  );
+  return (
+    <>
+        <button type="Onclick">
+          PAGE D'OUVERTURE
+        </button>
+    </>
+  )
 }
 
-export default App;
-
-
+export default App
--- PIM/src/main.jsx
+++ PIM/src/main.jsx
@@ -2,25 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter, RouterProvider,} from "react-router-dom";
-
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/Accueil",
-    element: <Accueil/>,
-  },
-]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  <RouterProvider router={router} />
+  
 )
