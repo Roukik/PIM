c99a875451d0749ab4a18c79cdc23c3129a3ee75
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-15 17:05:08 +0200

    +

---
+++ PIM/src/compenents/Menu.jsx
@@ -0,0 +1,29 @@
+/*import '../src/compenents/Menu.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Accueil from './pages/Accueil';
+import Favories from './pages/Favories';
+import Notifications from './pages/Notifications';
+
+import Product from './pages/Product';
+
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<Dashboard />} />
+          <Route path="about" element={<About />} />
+          <Route path="comment" element={<Comment />} />
+          <Route path="analytics" element={<Analystics />} />
+          <Route path="product" element={<Product />} />
+          <Route path="products" element={<ProductList />} />
+        </Route>
+      </Routes>
+
+    </BrowserRouter>
+  );
+};
+
+export default App;*/
