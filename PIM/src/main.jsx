a6eeb7a5bd3455cbdac2b73dce8804e118114fdb
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-16 20:00:38 +0200

    c'est carré le d

--- PIM/src/main.jsx
+++ PIM/src/main.jsx
@@ -1,38 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Menu from './components/Menu.jsx';
-import Favories from './Pages/Favories.jsx';
-import Notifications from './Pages/Notifications.jsx';
-import Profil from './Pages/Profil.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>
-  },
-  {
-    path: "/",
-    element: <Menu/>
-  },
-  {
-    path: "/",
-    element: <Notifications/>,
-  },
-  {
-    path: "/",
-    element: <Profil/>,
-  },
-  {
-    path: "/",
-    element: <Favories/>,
-  },
-]);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import Menu from './components/Menu';
+import Favories from './Pages/Favories';
+import Notifications from './Pages/Notifications';
+import Profil from './Pages/Profil';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <App></App>
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/profil" element={<Profil />} />
+        <Route path="/favorites" element={<Favories />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
