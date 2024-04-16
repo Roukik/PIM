11c4fe2101f6a5df8f4f99b885ce9d0251460727
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-16 14:29:50 +0200

    .

--- PIM/src/App.jsx
+++ PIM/src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Accueil from './Pages/Accueil';
+import Favories from './Pages/Favories';
+import Notifications from './Pages/Notifications';
+import PageOuverture from './Pages/PageOuverture';
+import Profil from './Pages/Profil';
+import Layout from './Layouts/Layout';
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <Link to='/Accueil'>PAGE D'OUVERTURE</Link>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="accueil" element={<Accueil />} />
+          <Route path="favories" element={<Favories />} />
+          <Route path="notifications" element={<Notifications />} />
+          <Route path="pageouverture" element={<PageOuverture />} />
+          <Route path="profil" element={<Profil />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
