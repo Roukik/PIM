04aec3f5e4fb6c08a5b778f9a01b816bae87e253
dienabadhb <d_diakhaby@etu-webschoolfactory.fr>
2024-04-16 16:26:02 +0200

    V

--- PIM/src/App.jsx
+++ PIM/src/App.jsx
@@ -7,20 +7,13 @@ import PageOuverture from './Pages/PageOuverture';
 import Profil from './Pages/Profil';
 import Layout from './Layouts/Layout';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="accueil" element={<Accueil />} />
-          <Route path="favories" element={<Favories />} />
-          <Route path="notifications" element={<Notifications />} />
-          <Route path="pageouverture" element={<PageOuverture />} />
-          <Route path="profil" element={<Profil />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
 
-export default App;
+function App() {
+  return (
+    <div>
+      <Menu></Menu>
+    </div>
+  )
+}
+
+export default App
--- PIM/src/components/Menu.jsx
+++ PIM/src/components/Menu.jsx
@@ -1,29 +1,11 @@
-/*import '../src/compenents/Menu.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Accueil from './pages/Accueil';
-import Favories from './pages/Favories';
-import Notifications from './pages/Notifications';
+import React from 'react'
 
-import Product from './pages/Product';
-
-
-
-const App = () => {
+function Menu() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<Dashboard />} />
-          <Route path="about" element={<About />} />
-          <Route path="comment" element={<Comment />} />
-          <Route path="analytics" element={<Analystics />} />
-          <Route path="product" element={<Product />} />
-          <Route path="products" element={<ProductList />} />
-        </Route>
-      </Routes>
+    <div>
+      
+    </div>
+  )
+}
 
-    </BrowserRouter>
-  );
-};
-
-export default App;*/
+export default Menu
