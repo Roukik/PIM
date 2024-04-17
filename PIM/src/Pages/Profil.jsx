70ccc8ae8d770b7904ca43a0e5e9cfbaf01de040
dienabadhb <d_diakhaby@etu-webschoolfactory.fr>
2024-04-17 19:06:12 +0200

    PP

--- PIM/src/Pages/Profil.jsx
+++ PIM/src/Pages/Profil.jsx
@@ -4,6 +4,7 @@ import FondChien from '../assets/fondChien.jpg'
 import './Profil'
 import '../Pages/Profil.css'
 import Fleche from '../assets/fleche.svg'
+import PP from '../assets/PP.png'
 
 
 
@@ -13,10 +14,21 @@ function Profil() {
       <Menu></Menu>
         { <img src={FondChien} alt="" className='fond' /> }
         <div className='block1'>
-          <div className=''>
+
+          <div className='Container1'>
             <img src={Fleche} alt="" className='arrow' />
             <p>Profil</p>
           </div>
+
+          <div className="Container2">
+            { <img src={PP} alt="" srcset="" className='PP' /> }
+          </div>
+
+          <dic className="Container3">
+            <p>Julien Rio</p>
+            <p>32 ans</p>
+          </dic>
+          
           <div className='block2'>
 
           </div>
