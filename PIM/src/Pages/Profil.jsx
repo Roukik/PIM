cdf187c64f3f3c8d8d0a8e5fe421d4968b20cb60
dienabadhb <d_diakhaby@etu-webschoolfactory.fr>
2024-04-17 17:14:45 +0200

    page profil

--- PIM/src/Pages/Profil.jsx
+++ PIM/src/Pages/Profil.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Menu from '../components/Menu'
-import '../assets/fondChien.jpg'
+import FondChien from '../assets/fondChien.jpg'
 import './Profil'
 import '../Pages/Profil.css'
+import Fleche from '../assets/fleche.svg'
 
 
 
@@ -10,10 +11,18 @@ function Profil() {
   return (
     <div className='ContainerProfil'>
       <Menu></Menu>
+        { <img src={FondChien} alt="" className='fond' /> }
         <div className='block1'>
+          <div className=''>
+            <img src={Fleche} alt="" className='arrow' />
+            <p>Profil</p>
+          </div>
+          <div className='block2'>
 
+          </div>
         </div>
     </div>
+    
   )
 }
 
