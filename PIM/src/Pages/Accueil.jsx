3840b588babbd5bf8ecfcdcdf032fdea4b77257f
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-18 14:39:12 +0200

    +

--- PIM/src/Pages/Accueil.jsx
+++ PIM/src/Pages/Accueil.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Menu from '../components/Menu';
-import '../Pages/Accueil.css'; // Import du fichier CSS externe
+import '../Pages/Accueil.css';
 import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
@@ -11,6 +11,9 @@ import Stack from '@mui/material/Stack';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import Scoopie from '../assets/scoopie.svg';
+import Riley from '../assets/Riley.svg';
+import Glue from '../assets/Glue.svg';
+import Rikers from '../assets/Rikers.svg';
 
 const Circle = {
   width: '50px',
@@ -39,6 +42,7 @@ function Accueil() {
       <Menu /> 
       <div className="espace">
         <p>Accueil</p>
+        {/* Première carte */}
         <Card sx={cardStyle}>
           <CardActionArea>
             <CardMedia
@@ -73,10 +77,123 @@ function Accueil() {
             </div>
           </CardContent>
         </Card>
+
+        {/* Deuxième carte (Riley) */}
+        <Card sx={cardStyle}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={Riley}
+              alt="Riley"
+              style={{ objectFit: 'cover', objectPosition: 'top' }}
+            />
+            <CardContent className="description">
+              <Typography gutterBottom variant="h7" component="div">
+                Riley, 3 ans
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Malinois
+                Agir Pour Les Animaux
+              </Typography>
+              <Stack direction="row" spacing={1}>
+                <Chip label="dynamique" className="chip" />
+                <Chip label="curieux" className="chip" />
+              </Stack>
+              <Stack direction="row" spacing={1}>
+                <Chip label="aventureux" className="chip" />
+                <Chip label="peureux" className="chip" />
+              </Stack>
+            </CardContent>
+          </CardActionArea>
+          <CardContent sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <div style={Circle} onClick={toggleFavorite}> 
+              {isFavorite ? (
+                <FavoriteIcon style={{ color: '#ffffff' }} />
+              ) : (
+                <FavoriteBorderIcon style={{ color: '#ffffff' }} />
+              )}
+            </div>
+          </CardContent>
+        </Card>
+
+        {/* Troisième carte */}
+        <Card sx={cardStyle}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={Glue}
+              alt="Glue"
+              style={{ objectFit: 'cover', objectPosition: 'top' }}
+            />
+            <CardContent className="description">
+              <Typography gutterBottom variant="h7" component="div">
+                Glue, 3 ans
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                X Labrador
+                SPA de Toulouse
+              </Typography>
+              <Stack direction="row" spacing={1}>
+                <Chip label="méfiant" className="chip" />
+                <Chip label="joueur" className="chip" />
+                <Chip label="affectueux" className="chip" />
+              </Stack>
+            </CardContent>
+          </CardActionArea>
+          <CardContent sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <div style={Circle} onClick={toggleFavorite}> 
+              {isFavorite ? (
+                <FavoriteIcon style={{ color: '#ffffff' }} />
+              ) : (
+                <FavoriteBorderIcon style={{ color: '#ffffff' }} />
+              )}
+            </div>
+          </CardContent>
+        </Card>
+
+        {/* Quatrième carte (Rikers) */}
+        <Card sx={cardStyle}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={Rikers}
+              alt="Rikers"
+              style={{ objectFit: 'cover', objectPosition: '' }} // Image centrée sur le haut
+            />
+            <CardContent className="description">
+              <Typography gutterBottom variant="h7" component="div">
+                Rikers, 3 ans
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                American Staffordshi
+                SPA de Toulouse
+              </Typography>
+              <Stack direction="row" spacing={1}>
+                <Chip label="attachant" className="chip" />
+                <Chip label="sociable" className="chip" />
+                <Chip label="joueur" className="chip" />
+              </Stack>
+            </CardContent>
+          </CardActionArea>
+          <CardContent sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <div style={Circle} onClick={toggleFavorite}> 
+              {isFavorite ? (
+                <FavoriteIcon style={{ color: '#ffffff' }} />
+              ) : (
+                <FavoriteBorderIcon style={{ color: '#ffffff' }} />
+              )}
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
 }
 
 export default Accueil;
+
+
 
