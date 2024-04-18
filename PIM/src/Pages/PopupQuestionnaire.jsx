40e8760af05ae42f2fd2457a251dd047c6a8296f
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-18 08:53:11 +0200

    +

---
+++ PIM/src/Pages/PopupQuestionnaire.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PopupQuestionnaire() {
+  return (
+    <div>PopupQuestionnaire</div>
+  )
+}
+
+export default PopupQuestionnaire
--- PIM/src/main.jsx
+++ PIM/src/main.jsx
@@ -10,6 +10,7 @@ import Profil from './Pages/Profil';
 import PageBienvenue from './Pages/PageBienvenue';
 import Popup from './Pages/Popup';
 import Accueil from './Pages/Accueil';
+import PopupQuestionnaire from './Pages/PopupQuestionnaire';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -18,6 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<PageBienvenue />} />
         <Route path="/popup" element={<Popup />} />
         <Route path="/accueil" element={<Accueil />} />
+        <Route path="/popupquestionnaire" element={<PopupQuestionnaire/>} />
         <Route path="/app" element={<App />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/notifications" element={<Notifications />} />
