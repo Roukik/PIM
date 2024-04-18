de7994a366533244770175b2be63747e9f1c950a
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-18 08:49:30 +0200

    c'esst carré
    connections des pages carréééééééééééééééééééééééééééééé

--- PIM/src/Pages/Popup.jsx
+++ PIM/src/Pages/Popup.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Tracepates from '../assets/tracepates.svg';
+import '../Pages/Popup.css';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Accueil from './Accueil';
+import { Link } from 'react-router-dom'; 
+
 
 function Popup() {
   return (
-    <div>Accueil</div>
-  )
+    <div className='Tracepates'>
+      <Link to="/Accueil">
+        <IconButton aria-label="close" className='CloseButton'>
+          <CloseIcon />
+        </IconButton>
+      </Link>
+      <img src={Tracepates} alt="" className="TracepatesImage" />
+    </div>
+  );
 }
 
-export default Popup
+export default Popup;
