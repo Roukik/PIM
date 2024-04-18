f28d7adee3bcdf1153a74c43b5b6fb816e6f952f
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-18 07:54:14 +0200

    boutonle
    bouton est rajouté

--- PIM/src/Pages/PageBienvenue.jsx
+++ PIM/src/Pages/PageBienvenue.jsx
@@ -5,13 +5,17 @@ import Button from '@mui/material/Button';
 
 function PageBienvenue() {
   return (
-    <div>
+    <div className="page-container">
       <h1 className="Bienvenue">Bienvenue</h1>
       <h2 className="eheh">Vous souhaitez :</h2>
+      <div className="button-adopte">
+        <Stack  spacing={2} direction="row" justifyContent="center" alignItems="center">
+          <Button variant="outlined" size="large">Cliquez ici</Button>
+        </Stack>
+      </div>
     </div>
   );
 }
 
 export default PageBienvenue;
-
 
