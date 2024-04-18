2eebe77c8cb18ae0408d1a7b3df236500ae1e778
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-18 07:42:44 +0200

    +

--- PIM/src/Pages/PageBienvenue.jsx
+++ PIM/src/Pages/PageBienvenue.jsx
@@ -1,42 +1,18 @@
-/*import React from 'react';
-import { Box, Button, Typography } from '@mui/material';
-import '../Pages/PageBienvenue.css';
+import React from 'react';
+import './PageBienvenue.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function PageBienvenue() {
-  const history = useHistory();
-
-  const handleClique = () => {
-    // Rediriger vers la page du popup lorsque le bouton est cliqué
-    history.push('/popup');
-  };
-
   return (
-    <Box className="pageBienvenueContainer"> 
-      <Typography variant="h3" gutterBottom>
-        Bienvenue !
-      </Typography>
-      <Typography variant="h5" gutterBottom>
-        Vous souhaitez :
-      </Typography>
-      <Button
-        variant="contained"
-        size="large"
-        onClick={handleClique}
-      >
-        Suivre le questionnaire
-      </Button>
-    </Box>
+    <div>
+      <h1 className="Bienvenue">Bienvenue</h1>
+      <h2 className="eheh">Vous souhaitez :</h2>
+    </div>
   );
 }
 
-export default PageBienvenue;*/
+export default PageBienvenue;
 
-import React from 'react'
 
-function PageBienvenue() {
-  return (
-    <div>PageBienvenue</div>
-  )
-}
 
-export default PageBienvenue
