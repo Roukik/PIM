bcda62d3e4212843f0c11d570d2b697b8df4fcdf
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-18 08:08:07 +0200

    Connection
    page bienvenue et popup connecté

--- PIM/src/Pages/PageBienvenue.jsx
+++ PIM/src/Pages/PageBienvenue.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './PageBienvenue.css';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Popup from './Popup'
 
 function PageBienvenue() {
   return (
@@ -9,8 +11,10 @@ function PageBienvenue() {
       <h1 className="Bienvenue">Bienvenue</h1>
       <h2 className="eheh">Vous souhaitez :</h2>
       <div className="button-adopte">
-        <Stack  spacing={2} direction="row" justifyContent="center" alignItems="center">
-          <Button variant="outlined" size="large">Cliquez ici</Button>
+        <Stack spacing={2} direction="row" justifyContent="center" alignItems="center">
+          <Link to="/Popup">
+            <Button variant="outlined" size="large">Adopter un animal dans le besoin</Button>
+          </Link>
         </Stack>
       </div>
     </div>
@@ -18,4 +22,6 @@ function PageBienvenue() {
 }
 
 export default PageBienvenue;
+
+
 
--- PIM/src/main.jsx
+++ PIM/src/main.jsx
@@ -8,12 +8,16 @@ import Favories from './Pages/Favories';
 import Notifications from './Pages/Notifications';
 import Profil from './Pages/Profil';
 import PageBienvenue from './Pages/PageBienvenue';
+import Popup from './Pages/Popup';
+import Accueil from './Pages/Accueil';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<PageBienvenue />} />
+        <Route path="/popup" element={<Popup />} />
+        <Route path="/accueil" element={<Accueil />} />
         <Route path="/app" element={<App />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/notifications" element={<Notifications />} />
