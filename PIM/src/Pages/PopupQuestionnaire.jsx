5c6d913c60007ab031b84f994d5f1aaaa1a3682b
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-18 17:02:43 +0200

    modification popupq

--- PIM/src/Pages/PopupQuestionnaire.jsx
+++ PIM/src/Pages/PopupQuestionnaire.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Q1 from '../assets/Q1.svg';
+import Button from '@mui/material/Button';
+import './PopupQuestionnaire.css';
 
 function PopupQuestionnaire() {
   return (
-    <div>PopupQuestionnaire</div>
-  )
+    <div className='i' style={{ position: 'relative' }}>
+      <img src={Q1} alt="" className='q1' />
+      <Button
+        variant="contained"
+        disableElevation
+        style={{
+          position: 'absolute',
+          bottom: '30px', 
+          left: '50%',
+          transform: 'translateX(-50%)',
+          width: '80%', 
+          borderRadius: '16px',
+          color: '#897F66',
+          backgroundColor: '#EAE3D3',
+          border: '2px solid #897F66',
+        }}
+      >
+        Suivant
+      </Button>
+    </div>
+  );
 }
 
-export default PopupQuestionnaire
+export default PopupQuestionnaire;
+
