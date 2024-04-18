84027ce03c287e20444cb0e221f5321e461247ea
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-18 09:47:01 +0200

    +

--- PIM/src/Pages/Popup.jsx
+++ PIM/src/Pages/Popup.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React from 'react'; // Importez React une seule fois
 import Tracepates from '../assets/tracepates.svg';
 import '../Pages/Popup.css';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
-import Accueil from './Accueil';
-import { Link } from 'react-router-dom'; 
-
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'; 
+import { Link } from 'react-router-dom';
+import PopupQuestionnaire from '../Pages/PopupQuestionnaire';
 
 function Popup() {
   return (
     <div className='Tracepates'>
+      <img src={Tracepates} alt="" className="TracepatesImage" />
       <Link to="/Accueil">
         <IconButton aria-label="close" className='CloseButton'>
-          <CloseIcon />
+          <CloseIcon style={{ fontSize: 48 }} />
         </IconButton>
       </Link>
-      <img src={Tracepates} alt="" className="TracepatesImage" />
+
+      <Link to="/PopupQuestionnaire">
+        <ArrowForwardIcon className="Arrow" style={{ fontSize: 48, color: 'black' }} />
+      </Link>
     </div>
   );
 }
 
 export default Popup;
+
