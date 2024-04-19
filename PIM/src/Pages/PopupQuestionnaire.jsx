7df8d5df233eb69ca861c0b8292b36be28e65e1b
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-19 17:37:58 +0200

    .

--- PIM/src/Pages/PopupQuestionnaire.jsx
+++ PIM/src/Pages/PopupQuestionnaire.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Q1 from '../assets/Q1.svg';
 import Button from '@mui/material/Button';
 import '../Pages/PopupQuestionnaire.css'; 
@@ -7,21 +7,41 @@ import ChatSansColor from '../assets/ChatSansColor.svg'
 import ChienSansColor from '../assets/ChienSansColor.svg'
 import HamsterSansColor from '../assets/HamsterSansColor.svg'
 import LapinSansColor from '../assets/LapinSansColor.svg'
+import ChatColor from '../assets/ChatColor.svg'
+import ChienColor from '../assets/chienColor.svg'
+import HamsterColor from '../assets/hamsterColor.svg'
+import LapinColor from '../assets/lapinColor.svg'
 
 function PopupQuestionnaire() {
+ 
+  const [likedImages, setLikedImages] = useState({
+    Chat: false,
+    Chien: false,
+    Hamster: false,
+    Lapin: false
+  });
+
+
+  const toggleLike = (imageName) => {
+    setLikedImages(prevState => ({
+      ...prevState,
+      [imageName]: !prevState[imageName]
+    }));
+  };
+
   return (
     <div className='i'>
       <img src={Q1} alt="" className='q1' />
       <div className="pets">
         <div className="pets12">
-          <img src={ChatSansColor} alt="" className="ChatSansColor" />
-          <img src={ChienSansColor} alt="" className="ChienSansColor" />
+          <img src={likedImages.Chat ? ChatColor : ChatSansColor} alt="" className="Chat" onClick={() => toggleLike("Chat")} />
+          <img src={likedImages.Chien ? ChienColor : ChienSansColor} alt="" className="Chien" onClick={() => toggleLike("Chien")} />
         </div>
 
         <div className="pets34">
           <div>
-            <img src={HamsterSansColor} alt="" className="HamsterSansColor" />
-            <img src={LapinSansColor} alt="" className="LapinSansColor" />
+            <img src={likedImages.Hamster ? HamsterColor : HamsterSansColor} alt="" className="Hamster" onClick={() => toggleLike("Hamster")} />
+            <img src={likedImages.Lapin ? LapinColor : LapinSansColor} alt="" className="Lapin" onClick={() => toggleLike("Lapin")} />
           </div>
         </div>
         <Link to="/Suivant" style={{ position: 'absolute', bottom: '30px', left: '50%', transform: 'translateX(-50%)', width: '80%' }}>
