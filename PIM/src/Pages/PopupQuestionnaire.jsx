db2cc90fc775f70825e28cea60e81c31813c07be
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-19 16:48:45 +0200

    +

--- PIM/src/Pages/PopupQuestionnaire.jsx
+++ PIM/src/Pages/PopupQuestionnaire.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Q1 from '../assets/Q1.svg';
 import Button from '@mui/material/Button';
-import './PopupQuestionnaire.css';
+import '../Pages/PopupQuestionnaire.css'; 
 import { Link } from 'react-router-dom';
 import ChatSansColor from '../assets/ChatSansColor.svg'
 import ChienSansColor from '../assets/ChienSansColor.svg'
@@ -10,63 +10,40 @@ import LapinSansColor from '../assets/LapinSansColor.svg'
 
 function PopupQuestionnaire() {
   return (
-    <div className='i' style={{ position: 'relative' }}>
+    <div className='i'>
       <img src={Q1} alt="" className='q1' />
       <div className="pets">
-    <div className="pets12">
-      <img src={ChatSansColor} alt="" className="ChatSansColor" />
-      <img src={ChienSansColor} alt="" className="ChienSansColor" />
-    </div>
+        <div className="pets12">
+          <img src={ChatSansColor} alt="" className="ChatSansColor" />
+          <img src={ChienSansColor} alt="" className="ChienSansColor" />
+        </div>
 
-    <div className="pets34">
-      <div>
-        <img src={HamsterSansColor} alt="" className="HamsterSansColor" />
-        <img src={LapinSansColor} alt="" className="LapinSansColor" />
-     </div>
-     </div>
-           <Link to="/Suivant">
-      <Button
-        variant="contained"
-        disableElevation
-        style={{
-          position: 'absolute',
-          bottom: '30px', 
-          left: '50%',
-          transform: 'translateX(-50%)',
-          width: '80%', 
-          borderRadius: '16px',
-          color: '#897F66',
-          backgroundColor: '#EAE3D3',
-          border: '2px solid #897F66',
-        }}
-      >
-        Suivant
-      </Button>
-      </Link>
-    
-   </div>
-      <Link to="/Suivant">
-      <Button
-        variant="contained"
-        disableElevation
-        style={{
-          position: 'absolute',
-          bottom: '30px', 
-          left: '50%',
-          transform: 'translateX(-50%)',
-          width: '80%', 
-          borderRadius: '16px',
-          color: '#897F66',
-          backgroundColor: '#EAE3D3',
-          border: '2px solid #897F66',
-        }}
-      >
-        Suivant
-      </Button>
-      </Link>
+        <div className="pets34">
+          <div>
+            <img src={HamsterSansColor} alt="" className="HamsterSansColor" />
+            <img src={LapinSansColor} alt="" className="LapinSansColor" />
+          </div>
+        </div>
+        <Link to="/Suivant" style={{ position: 'absolute', bottom: '30px', left: '50%', transform: 'translateX(-50%)', width: '80%' }}>
+          <Button
+            variant="contained"
+            disableElevation
+            style={{
+              width: '100%',
+              borderRadius: '16px',
+              color: '#897F66',
+              backgroundColor: '#EAE3D3',
+              border: '2px solid #897F66',
+            }}
+          >
+            Suivant
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
 
 export default PopupQuestionnaire;
+
 
