988176427c46b8f8a4c48f959f941567351480c9
dienabadhb <d_diakhaby@etu-webschoolfactory.fr>
2024-04-19 16:19:17 +0200

    BarNotif

--- PIM/src/Pages/Notifications.jsx
+++ PIM/src/Pages/Notifications.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
-import Menu from '../components/Menu'
 import '../Pages/Notification.css'
 import Fleche from '../assets/fleche.svg'
 import Grid from '../components/Grid'
+import BarNotif from '../components/BarNotif'
 
 
 function Notifications() {
   return (
     <div>
-    
       <div className="Container1Notif">
         { <img src={Fleche} alt="" className='arrow' /> }
         <p>Notification</p>
       </div>
       <div className='Container2Notif'>
         <div className='Container3Notif'>
-          <p>Toutes</p>
-          <p>Non lu</p>
-        </div>
-        <div className='Container4Notif'>
-        <Grid></Grid>
+          <BarNotif></BarNotif>
         </div>
       </div>
     </div>
