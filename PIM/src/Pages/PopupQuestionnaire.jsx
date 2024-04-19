2af2d83d6396bac9e80294f4b556a6a7c86e7c9e
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-19 16:18:38 +0200

    .

--- PIM/src/Pages/PopupQuestionnaire.jsx
+++ PIM/src/Pages/PopupQuestionnaire.jsx
@@ -3,11 +3,48 @@ import Q1 from '../assets/Q1.svg';
 import Button from '@mui/material/Button';
 import './PopupQuestionnaire.css';
 import { Link } from 'react-router-dom';
+import ChatSansColor from '../assets/ChatSansColor.svg'
+import ChienSansColor from '../assets/ChienSansColor.svg'
+import HamsterSansColor from '../assets/HamsterSansColor.svg'
+import LapinSansColor from '../assets/LapinSansColor.svg'
 
 function PopupQuestionnaire() {
   return (
     <div className='i' style={{ position: 'relative' }}>
       <img src={Q1} alt="" className='q1' />
+      <div className="pets">
+    <div className="pets12">
+      <img src={ChatSansColor} alt="" className="ChatSansColor" />
+      <img src={ChienSansColor} alt="" className="ChienSansColor" />
+    </div>
+
+    <div className="pets34">
+      <div>
+        <img src={HamsterSansColor} alt="" className="HamsterSansColor" />
+        <img src={LapinSansColor} alt="" className="LapinSansColor" />
+     </div>
+     </div>
+           <Link to="/Suivant">
+      <Button
+        variant="contained"
+        disableElevation
+        style={{
+          position: 'absolute',
+          bottom: '30px', 
+          left: '50%',
+          transform: 'translateX(-50%)',
+          width: '80%', 
+          borderRadius: '16px',
+          color: '#897F66',
+          backgroundColor: '#EAE3D3',
+          border: '2px solid #897F66',
+        }}
+      >
+        Suivant
+      </Button>
+      </Link>
+    
+   </div>
       <Link to="/Suivant">
       <Button
         variant="contained"
