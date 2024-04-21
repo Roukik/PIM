6d91cb3ae2ce435eb1b8b47a8a8248e39128d81d
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-21 11:40:29 +0200

    +

--- PIM/src/BackPage/P1.jsx
+++ PIM/src/BackPage/P1.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import PQRefuge from "../assets/PQRefuge.png"
-
+import React from 'react';
+import PQRefuge from "../assets/PQRefuge.png";
+import { IconButton } from '@mui/material';
+import { ArrowForward } from '@mui/icons-material';
+import './P1.css'; // Fichier de style CSS pour vos personnalisations
 
 function P1() {
   return (
-    <div>
-        <img src={PQRefuge} alt="" className='Fond'/>
+    <div className="container">
+      <div className="background">
+        <img src={PQRefuge} alt="" className="image"/>
+        <div className="arrow-container">
+          <IconButton className="arrow-button">
+            <ArrowForward style={{ fontSize: 250, color: 'white' }}/>
+          </IconButton>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default P1
+export default P1;
+
