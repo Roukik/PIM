26fa0b6d26122071affb595285eb7e29de8e68b9
Roukik <148196386+Roukik@users.noreply.github.com>
2024-04-22 11:20:52 +0200

    7

--- PIM/src/Pages/PagesRDV.jsx
+++ PIM/src/Pages/PagesRDV.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import Fleche from '../assets/fleche.svg'
-import '../Pages/RDV.css'
-import Refugeimg from '../assets/ScootieMagelan.png'
-import RDVCard from '../components/RDVCard'
+import React from 'react';
+import Fleche from '../assets/fleche.svg';
+import '../Pages/RDV.css';
+import Refugeimg from '../assets/ScootieMagelan.png';
+import RDVCard from '../components/RDVCard';
+import DateCalendarValue from "../components/DateCalendarValue";
 
 
 function PagesRDV() {
   return (
     <div className="Container">
-        <div className="Container1RDV">
+      <div className="Container1RDV">
         <img src={Fleche} alt="" className='arrow' />
-           <p>Description</p>
-        </div>
-        <div className="Containe2RDV">
-          <img src={Refugeimg} alt="" className='Refugeimg'/>
-          <div className="Container3RDV">
-            <RDVCard></RDVCard>
-            
-          </div>
-        </div>
+        <p>Description</p>
+      </div>
+      <div className="Containe2RDV">
+        <img src={Refugeimg} alt="" className='Refugeimg'/>
+        <div className="Container3RDV">
+          <RDVCard />
 
+          <DateCalendarValue />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PagesRDV
+export default PagesRDV;
+
+
+
+
+
+
+
